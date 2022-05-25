1d0fce42eb1b7f160548bd1d55d8c61b02036a17
sasaki <sasaki.tatsuya@headwaters.co.jp>
2022-05-25 23:53:51 +0900

    apply materia-ui

--- src/components/task_list.js
+++ src/components/task_list.js
@@ -1,6 +1,22 @@
 import React, { useContext } from "react";
+import Button from '@mui/material/Button';
+import { DataGrid } from '@mui/x-data-grid';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 import { TaskContext } from "./home";
+
+const columns = [
+  { field: 'deadline', headerName: 'deadline', width: 70 },
+  { field: 'task_state', headerName: 'state', width: 130 },
+  { field: 'content', headerName: 'Last name', width: 130 },
+  { field: 'delete', headerName: '⚙', width: 90, }
+];
 
 function TaskList() {
   const { tasks, setTasks } = useContext(TaskContext);
@@ -16,31 +32,37 @@ function TaskList() {
     return tasks.map((currentTask) => {
       console.log(currentTask)
       return (
-        <tr key={currentTask.id}>
-          <td>{currentTask.deadline}</td>
-          <td>{currentTask.task_state}</td>
-          <td>{currentTask.content}</td>
-          <td><button onClick={e => handleClick(e, currentTask.id)}>delete</button></td>
-        </tr>
+        <TableRow
+          key={currentTask.id}
+          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+        >
+          <TableCell component="th" scope="row">
+            {currentTask.deadline}
+          </TableCell>
+          <TableCell>{currentTask.task_state}</TableCell>
+          <TableCell>{currentTask.content}</TableCell>
+          <TableCell align="center"><Button variant="contained" onClick={e => handleClick(e, currentTask.id)}>delete</Button></TableCell>
+        </TableRow>
       )
     })
   }
 
   return (
-    <table border="1">
-      <thead>
-        <tr>
-          <th>deadline</th>
-          <th>task_state</th>
-          <th>content</th>
-          <th>⚙</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {renderTasks()}
-      </tbody>
-    </table>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>deadline</TableCell>
+            <TableCell>task_state</TableCell>
+            <TableCell>content</TableCell>
+            <TableCell align="center" style={{width:"100px"}}>⚙</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {renderTasks()}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
--- src/components/task_post.js
+++ src/components/task_post.js
@@ -1,5 +1,13 @@
 import React, { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 import { TaskContext } from "./home";
 
@@ -8,7 +16,7 @@ function TaskPost() {
     const { tasks, setTasks } = useContext(TaskContext);
     const [ id, setId ] = useState(0);
     
-    let task_history = tasks ? tasks : []
+    let task_history = tasks ? [...tasks] : []
 
     const onSubmit = data => {
         const { deadline, task_state, content } = data
@@ -23,26 +31,62 @@ function TaskPost() {
         setTasks([...task_history])  // スプレッド構文にしないとsetTasksで値が更新されない。
     }
 
+    const handleChange = (value) => {
+        console.log(value)
+    }
+
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            deadline: 
-            <input type="date" {...register("deadline")} />
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2} alignItems="center" justifyContent="center">
+                    <Grid item xs={2}>
+                        <TextField fullWidth type="date" {...register("deadline")} />
+                    </Grid>
 
-            State:
-            <select {...register("task_state", { required: true })}>
-                <option value="not-started">not started</option>
-                <option value="in-progress">in progress</option>
-                <option value="done">done</option>
-            </select>
-            {errors.task_state && <span>This field is required</span>}
-            
-            {/* include validation with required or other standard HTML validation rules */}
-            Content: 
-            <input {...register("content")} />
-            {/* errors will return when field validation fails  */}
-            {/* {errors.exampleRequired && <span>This field is required</span>} */}
-            
-            <input type="submit" />
+                    <Grid item xs={2}>
+                        <FormControl fullWidth>
+                            <InputLabel>state</InputLabel>
+                            <Select 
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                defaultValue={""}
+                                label="state"
+                                onChange={handleChange}
+                                {...register("task_state", { required: true })}
+                            >
+                                <MenuItem value={"not-started"}>not started</MenuItem>
+                                <MenuItem value={"in-progress"}>in progress</MenuItem>
+                                <MenuItem value={"done"}>done</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Grid>
+
+                    <Grid item xs={2}>
+                        <TextField fullWidth {...register("content")} />
+                    </Grid>
+
+                    <Grid item xs={1} alignItems="center">
+                        <Button type="submit" variant="contained">add</Button>
+                    </Grid>
+
+                </Grid>
+                <Grid container spacing={2} alignItems="baseline" justifyContent="center">
+                    <Grid item xs={2}>
+                        <div style={{height:"50px"}}>　</div>
+                    </Grid>
+
+                    <Grid item xs={2}>
+                            {errors.task_state && <span>This field is required</span>}
+                    </Grid>
+
+                    <Grid item xs={2}>
+                    </Grid>
+
+                    <Grid item xs={1} alignItems="center">
+                    </Grid>
+
+                </Grid>
+            </Box>
         </form>
     )
 }
